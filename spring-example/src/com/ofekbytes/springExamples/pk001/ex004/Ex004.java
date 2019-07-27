cd66de6e843295ce21045af022acd4f2d8fbe10a
yaron <koskas@yahoo.com>
2019-07-27 18:50:48 +0300

    example 4 start

---
+++ spring-example/src/com/ofekbytes/springExamples/pk001/ex004/Ex004.java
@@ -0,0 +1,13 @@
+package com.ofekbytes.springExamples.pk001.ex004;
+
+public class Ex004 {
+	// main //
+	public static void main(String[] args) {
+		
+		//create object
+		Coach bbc = new TrackCoach();
+		
+		//use the object
+		System.out.println( bbc.getDailyWorkout() );
+	}
+}
---
+++ spring-example/src/com/ofekbytes/springExamples/pk001/ex004/TrackCoach.java
@@ -0,0 +1,10 @@
+package com.ofekbytes.springExamples.pk001.ex004;
+
+public class TrackCoach implements Coach {
+
+	@Override
+	public String getDailyWorkout() {
+		return "run 10 kilometers";
+	}
+
+}
