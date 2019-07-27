afc456a8b22955a2be90f8ae25e0624be0078aa0
yaron <koskas@yahoo.com>
2019-07-27 09:12:16 +0300

    test

---
+++ spring-example/src/com/ofekbytes/springExamples/pk001/ex001/BaseballCoach.java
@@ -0,0 +1,10 @@
+package com.ofekbytes.springExamples.pk001.ex001;
+
+public class BaseballCoach {
+
+	public void getDailyWorkout() {
+		System.out.println("start Daily workout");
+	}
+	
+	
+}
---
+++ spring-example/src/com/ofekbytes/springExamples/pk001/ex001/Ex001.java
@@ -0,0 +1,9 @@
+package com.ofekbytes.springExamples.pk001.ex001;
+
+public class Ex001 {
+	// main //
+	public static void main(String[] args) {
+		BaseballCoach bbc = new BaseballCoach();
+		bbc.getDailyWorkout();
+	}
+}
